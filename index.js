c2fd133fdefbf00f8d0e29dd86ffd886a85351ad
Wieland Hilker <88767090+monomango-wieland-hilker@users.noreply.github.com>
2022-04-20 16:02:18 +0200

    initital commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const port = process.env.PORT || 8000
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('This is running in a container.');
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
